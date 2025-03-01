97d13f708eb617c640301507dae8d9476c0c3212
arefinMilen <arefin15-5279@diu.edu.bd>
2025-03-02 02:44:08 +0600

    desing and implementation

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,20 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
  
- const todoTitle = 'My first todo list';
+//  const todoTitle = 'My first todo list';
  const date = new Date();
  const currentTime = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
  const currentDate = date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear();
+ 
 
+ const headingStyle = {
+  color: 'blue',
+  fontSize: '30px',
+  color: 'blue',
+  backgroundColor: 'yellow',
+  padding: '10px',
+  textAlign: 'center'
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <div>
-    <h1> Hello, world! </h1>
-    <h2>{todoTitle}</h2>
-    <p>Current time: {currentTime}</p>
-    <p>Current date: {currentDate}</p>
-    <p>lorem ipsum dolor sit amet...
+  <div >
+    <h1 className='headerStyle' style={headingStyle}> Todo-APP</h1>
+    <h2></h2>
+    <div className='card'>
+    <p className='cardTitle'>Current time: {currentTime}</p>
+    <p className='cardDesc'>Current date: {currentDate}</p>
+    <p className='cardFooter'>lorem ipsum dolor sit amet...
     </p>
+    </div>
   </div>
 );
 

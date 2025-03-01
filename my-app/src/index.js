d9f476ab7de1af6ad1379768ada0be335480c2d3
arefinMilen <arefin15-5279@diu.edu.bd>
2025-03-02 02:09:04 +0600

    jsx and js expression

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
  
- 
+ const todoTitle = 'My first todo list';
+ const date = new Date();
+ const currentTime = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
+ const currentDate = date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<h1> lets see if this works </h1>);
+root.render(
+  <div>
+    <h1> Hello, world! </h1>
+    <h2>{todoTitle}</h2>
+    <p>Current time: {currentTime}</p>
+    <p>Current date: {currentDate}</p>
+    <p>lorem ipsum dolor sit amet...
+    </p>
+  </div>
+);
 
  

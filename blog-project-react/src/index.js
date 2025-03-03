b4fd4c29da8909a5b9f673755f534a01e9186392
arefinMilen <arefin15-5279@diu.edu.bd>
2025-03-03 23:32:25 +0600

    react 17 old version

---
+++ blog-project-react/src/index.js
@@ -0,0 +1,15 @@
+import React  from "react";
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+{
+    color: "red";
+    fontsize: "20px";
+}
+root.render(
+    <div>
+    <h1>welcome react</h1>
+    <p>welcome to my blog</p>
+    </div>,
+);

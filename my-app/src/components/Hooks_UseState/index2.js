4f9c04db00f279c275b7b4b411756522dd5e07e3
arefinMilen <arefin15-5279@diu.edu.bd>
2025-04-08 00:25:22 +0600

    using prevState for count previous values

--- my-app/src/components/Hooks_UseState/index2.js
+++ my-app/src/components/Hooks_UseState/index2.js
@@ -3,7 +3,9 @@ import React,{useState} from 'react'
 export default function Hook_State2() {
     const [count, setCount] = useState(0)
     const handleIncrement = () => {
-        setCount(count + 1);
+        setCount((prevCount) => prevCount + 1);
+        setCount((prevCount) => prevCount + 1);
+        
     }
   return (
     <div>

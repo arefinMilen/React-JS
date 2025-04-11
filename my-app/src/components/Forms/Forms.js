afd02c80a24ecc3d3edbbc7199845fff8c5cb8b4
arefinMilen <arefin15-5279@diu.edu.bd>
2025-04-12 01:04:00 +0600

    update useState with objects

--- my-app/src/components/Forms/Forms.js
+++ my-app/src/components/Forms/Forms.js
@@ -6,37 +6,30 @@ export default function Forms() {
     // const [email,setEmail] = useState('');
     // const [password,setPassword] = useState('');
     
-    const [user,setUser] = useState({name:'', password:'', email:''})
+    const [user,setUser] = useState({name: '', password: '', email: ''})
     const {name, email, password} = user
      
-     
-  
-         
-
-        const handleChange = (e) => {
-            setUser({...user,[e.target.name]:e.target.value})
-        }
+    const handleChange = (e) => {
+            setUser({...user, [e.target.name]: e.target.value });
+            // console.log(e.target.name);
+        };
         
     const handleSubmit = (e) => {
         console.log("form is submitted");
-        // let userInfo = {
-        //     name,
-        //     email,
-        //     password
-        // }
         console.log(user);
-        e.preventDefault();}
+        e.preventDefault();
+    };
         
   return (
        <div className='style.formGroup'>
       <h2>Registration forms: </h2>
       <form action="" onSubmit={handleSubmit}>
         <label htmlFor="name">Name</label>
-        <input type="text" placeholder='Enter your name' id='name' value={name} onChange={handleChange}/> <br />
+        <input type="text" name="name" placeholder='Enter your name' id='name' value={name} onChange={handleChange}/> <br />
         <label htmlFor="email">Email</label>
-        <input type="email" placeholder='Enter your email' id='email' value={email} onChange={handleChange} /> <br />
+        <input type="email" name="email" placeholder='Enter your email' id='email' value={email} onChange={handleChange} /> <br />
         <label htmlFor="password">Password</label>
-        <input type="password" placeholder='Enter your password' id='password' value={password} onChange={handleChange} /> <br />
+        <input type="password"  name="password" placeholder='Enter your password' id='password' value={password} onChange={handleChange} /> <br />
         <button type='submit'>Register</button>
       </form>
     </div>

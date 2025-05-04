b6c99c8b0b222ea004c41e894de4b9811fe62964
arefinMilen <arefin15-5279@diu.edu.bd>
2025-05-05 01:15:57 +0600

    using useEffect with basic example

--- form-validation/src/App.js
+++ form-validation/src/App.js
@@ -4,13 +4,15 @@ import SignUP from './component/SignUp';
 import Toggle from './component/Toggle';  
 import './App.css';
 import FAQS from './component/FAQ/FAQS';
+import UseEffectExample from './component/UseEffectExample';
 
 function App() {
   return (
-    <div  >
+    <div>
       <SignUP />
       <Toggle />
       <FAQS />
+      <UseEffectExample />
     </div>
   );
 }
---
+++ form-validation/src/component/UseEffectExample.js
@@ -0,0 +1,32 @@
+import React,{useState,useEffect} from 'react'
+
+
+const UseEffectExample = () => {
+    const [count, setCount] = useState(0);
+    const [name,setName] = useState("false")
+    useEffect(()=>{
+        console.log("useFffect called");
+    },[count])
+    
+  return (
+    
+    <div>
+       {console.log("rendering component")}
+      <h1 style={{textAlign:"start"}}>Count:{count}</h1>
+      <button
+       onClick={()=>{
+        setCount((count)=>count+1);
+        }}
+        >+</button>
+      <button
+       onClick={()=>{
+        setName(!name);
+        }}
+        >Name</button>
+
+        <br/>
+    </div>
+  )
+}
+
+export default UseEffectExample
